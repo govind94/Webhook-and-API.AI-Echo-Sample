4445689b8e528a4fdc2ef2b47fef864efa7fa9bf
Govindan Vedanarayanan <vamsivedanarayanan@gmail.com>
2017-02-27 15:36:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    var speech = '';
+    if (req.body.result.metadata.intentName === 'weather')
+        speech = 'weather intent was called';
+    else if (req.body.result.metadata.intentName === 'directions')
+        speech = 'directions intent was called';
+    else speech = 'No intent was called';
     return res.json({
         speech: speech,
         displayText: speech,
